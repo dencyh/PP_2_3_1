0e76b712150bf7883ac9c03ba5e461511b796b21
Alexander Moryan <a.dencyh@gmail.com>
2023-02-06 23:42:29 +0300

    fix: add utf8 support

--- src/main/java/web/config/AppInit.java
+++ src/main/java/web/config/AppInit.java
@@ -1,8 +1,12 @@
 package web.config;
 
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.filter.DelegatingFilterProxy;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+import javax.servlet.Filter;
+import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
@@ -30,13 +34,26 @@ public class AppInit extends AbstractAnnotationConfigDispatcherServletInitialize
 		return new String[]{"/"};
 	}
 
-
 	@Override
 	public void onStartup(ServletContext servletContext) throws ServletException {
 		super.onStartup(servletContext);
-		servletContext
-			.addFilter("hiddenHttpMethodFilter", new HiddenHttpMethodFilter())
-			.addMappingForUrlPatterns(null, true, "/*");
+
+		FilterRegistration.Dynamic encodingFilter = servletContext.addFilter(
+			"encodingFilter",
+			new CharacterEncodingFilter()
+		);
+		encodingFilter.setInitParameter("encoding", "UTF-8");
+		encodingFilter.setInitParameter("forceEncoding", "true");
+		encodingFilter.addMappingForUrlPatterns(null, true, "/*");
+
+		registerHiddenFieldFilter(servletContext);
+	}
+
+	private void registerHiddenFieldFilter(ServletContext aContext) {
+		aContext.addFilter(
+			"hiddenHttpMethodFilter",
+			new HiddenHttpMethodFilter()
+		).addMappingForUrlPatterns(null, true, "/*");
 	}
 
 }
